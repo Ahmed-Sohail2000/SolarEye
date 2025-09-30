63106baabeb778ff2bf7c1846340fe0116b545b6
Ahmed-Sohail2000 <107795296+Ahmed-Sohail2000@users.noreply.github.com>
2025-09-30 12:21:09 +0200

    added an if statement in the index file to create a new site button if the sites are empty

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -1,22 +1,40 @@
 <!DOCTYPE html> 
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">  
-    <title>Solar Eye | Home</title>
-</head>
-<body>
-    <h1>Currently available sites</h1>
-    <p>List of all sites</p>
+    
+    <head>
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">  
+            
+            <title>Solar Eye | Home</title>
+    
+    </head>
+    <body>
+        
+        <h1>Currently available sites</h1>
+        <p>List of all sites</p>
 
-    <ol>
-        @foreach($sites as $site) <!-- Loop through each site from the  -->
-            <li>
-                <a href="/sites/{{ $site->id }}"> {{ $site->name}}</a> <!-- Link to the show page for each site -->
-            </li>
-        @endforeach
-    </ol>
+            @if($sites->isEmpty()) <!--Check if there is any empty sites-->
+                
+                <p> No sites available </p>
 
-</body>
-</html>
+                <a href = "/sites/create", class = "btn"> Create a new site </a> <!--Link to create a new site-->
+                
+            @else
+            
+                <olL> <!--Ordered list to display the sites-->
+                    @foreach($sites as $site) <!-- Loop through each site from the  -->   
+                        
+                        <li>
+                            <a href="/sites/{{ $site->id }}"> {{ $site->name}}</a> <!-- Link to the site detail page for each site -->
+                        </li>
+                    
+                    @endforeach
+                
+                </ol>
+            
+            @endif
 
+    </body>    
+    </html>
