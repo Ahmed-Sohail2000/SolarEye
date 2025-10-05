4450bf9d23134d22709504957bf19da22e240f38
Ahmed-Sohail2000 <107795296+Ahmed-Sohail2000@users.noreply.github.com>
2025-10-05 17:20:24 +0200

    modified the app.css and made small css styling

--- resources/views/components/page-layout.blade.php
+++ resources/views/components/page-layout.blade.php
@@ -3,6 +3,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Welcome to SolarEye </title>
+
+    @vite('resources/css/app.css') <!-- Link to the compiled CSS file using Vite -->
+    
 </head>
 <body>
     
