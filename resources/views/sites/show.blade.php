c0c661a09f4b677d685e6a4225d99d512af2be73
Ahmed-Sohail2000 <107795296+Ahmed-Sohail2000@users.noreply.github.com>
2025-10-08 15:56:16 +0200

    added the success message in the show file

--- resources/views/sites/show.blade.php
+++ resources/views/sites/show.blade.php
@@ -10,7 +10,12 @@
         <p> Location: {{$site->latitude}}, {{$site->longitude}} </p> <!--Display the latitude and longitude of the site-->
         
         <p> Created at: {{$site->created_at}} </p> <!--Display the creation date of the site-->
-    
+        
+        @if(session('success'))
+        <div class="bg-green-100 text-green-700 p-3 rounded mb-4">
+            {{ session('success') }}
+        </div>
+    @endif
     </div>
     
     <p><a href = "/sites"> Back to all sites </a></p> 
