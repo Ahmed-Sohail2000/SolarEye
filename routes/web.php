2d37de5c892ab88103399753cbb6b21475c14d7a
Ahmed-Sohail2000 <107795296+Ahmed-Sohail2000@users.noreply.github.com>
2025-10-09 17:45:12 +0200

    change the view of the route to welcome and fixed the login and logout to redirect to the welcome

--- routes/web.php
+++ routes/web.php
@@ -12,39 +12,38 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::view('/', 'welcome')->middleware(['auth', 'verified'])->name('welcome');
 
 Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
+Route::redirect('settings', 'settings/profile');
 
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+Route::get('settings/profile', Profile::class)->name('settings.profile');
+Route::get('settings/password', Password::class)->name('settings.password');
+Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 
-    ## SITE CRUD ROUTES
+## SITE CRUD ROUTES
 
-    # create a route request for the SITE page
-    Route::get('/sites', [SiteController::class, 'index']) -> name('sites.index'); # main page for all sites
+# create a route request for the SITE page
+Route::get('/sites', [SiteController::class, 'index']) -> name('sites.index'); # main page for all sites
 
-    # create a route request for the create new site page, store, remove, update, and edit 
-    Route::get('/sites/create', [SiteController::class, 'create']) -> name('sites.create'); # create a new site page form)
+# create a route request for the create new site page, store, remove, update, and edit 
+Route::get('/sites/create', [SiteController::class, 'create']) -> name('sites.create'); # create a new site page form)
 
-    # post route to store the new site data in the database
-    Route::post("/sites", [SiteController::class, 'store']) -> name('sites.store'); # store the new site data
+# post route to store the new site data in the database
+Route::post("/sites", [SiteController::class, 'store']) -> name('sites.store'); # store the new site data
     
-    # create a show route request for the SITE detail page
-    Route::get('/sites/{id}', [SiteController::class, 'show']) -> name('sites.show'); # detail page for each site with the id parameter of site
+# create a show route request for the SITE detail page
+Route::get('/sites/{id}', [SiteController::class, 'show']) -> name('sites.show'); # detail page for each site with the id parameter of site
 
-    # create an edit route request for the SITE edit page
-    Route::get('/sites/{id}/edit', [SiteController::class, 'edit']) -> name('sites.edit'); # edit the site data
+# create an edit route request for the SITE edit page
+Route::get('/sites/{id}/edit', [SiteController::class, 'edit']) -> name('sites.edit'); # edit the site data
 
-    # put route to update the site data in the database
-    Route::put('/sites/{id}', [SiteController::class, "update"])->name('sites.update'); # update the site data
+# put route to update the site data in the database
+Route::put('/sites/{id}', [SiteController::class, "update"])->name('sites.update'); # update the site data
 
-    # put route to delte the site data in the database
-    Route::delete('/sites/{id}', [SiteController::class, 'destroy'])->name('sites.destroy'); # delete the site data
+# put route to delte the site data in the database
+Route::delete('/sites/{id}', [SiteController::class, 'destroy'])->name('sites.destroy'); # delete the site data
+
 });
 
 require __DIR__.'/auth.php';
